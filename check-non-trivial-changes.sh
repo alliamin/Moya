a0c49a35fc4ec9eec758ce7819c3f140972c4179
aliamin <aamin@instabug.com>
2018-02-02 10:19:37 +0200

    modification

--- check-non-trivial-changes.sh
+++ check-non-trivial-changes.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-CHANGED_FILES=$(git show --name-only --pretty="" $CIRCLE_SHA1)
-
-if grep -qvE '(\.(md|yml)$)|(^(docs|web))/' <<< $CHANGED_FILES ; then
-  exit 0
-fi
-
-echo "Only docs were updated, stopping build!"
-exit 1
+result=$(ls -l | grep -i 's.version' Moya.podspec)
+echo ">>> $result"
